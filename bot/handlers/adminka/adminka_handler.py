640af111f647a121872c309c205383b44614147a
iSlavok <slavaterehin6@gmail.com>
2025-04-21 19:57:09 +0500

    Add mailing functionality to send messages to users in batches

--- bot/handlers/adminka/adminka_handler.py
+++ bot/handlers/adminka/adminka_handler.py
@@ -1,3 +1,6 @@
+import asyncio
+from contextlib import suppress
+
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
@@ -7,6 +10,7 @@ from bot.config import messages
 from bot.enums import UserRole
 from bot.filters import RoleFilter
 from bot.keyboards import get_adminka_kb
+from bot.services import UserService
 
 router = Router(name="adminka_router")
 
@@ -22,3 +26,24 @@ async def adminka_menu(event: Message | CallbackQuery, state: FSMContext):
     await state.clear()
     if isinstance(event, CallbackQuery):
         await event.answer()
+
+
+@router.message(
+    Command("mailing"),
+    F.reply_to_message,
+)
+async def mailing(message: Message, user_service: UserService):
+    total_count = user_service.get_users_count()
+    total_pages = (total_count // 100) + (1 if total_count % 100 > 0 else 0)
+    await message.answer("Рассылка начата")
+    good = 0
+    for page in range(1, total_pages + 1):
+        users = user_service.get_users(page=page, limit=100)
+        for user in users:
+            with suppress(Exception):
+                print(user)
+                await message.reply_to_message.send_copy(
+                    chat_id=user.id, reply_markup=message.reply_to_message.reply_markup)
+                good += 1
+            await asyncio.sleep(0.075)
+    await message.answer(text=f"Рассылка завершена, её получили {good} чел.")
