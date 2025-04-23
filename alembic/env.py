248adcdc3db3e5be08eb614df5327c45e1921675
iSlavok <slavaterehin6@gmail.com>
2025-04-23 17:51:37 +0500

    Remove dotenv dependency and update database URL configuration to use environment variable

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()  # noqa: E402
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -25,6 +25,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
@@ -43,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.getenv("DATABASE_URL")
     context.configure(
         url=url,
         target_metadata=target_metadata,
